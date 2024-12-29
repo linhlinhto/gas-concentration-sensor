9783110055e02f33ac06289fc127dd7825589c06
linhlinhto <118755644+linhlinhto@users.noreply.github.com>
2024-12-30 05:08:28 +0700

    Delete Gaswarning_code directory

--- Gaswarning_code/src/secrets/mqtt.h
+++
@@ -1,7 +0,0 @@
-namespace MQTT
-{
-    const char *username = "linh2";
-    const char *pass = "Hailinhqn1";
-    const char *broker = "25cc75707a0d4b5ba28127938feb1b9b.s1.eu.hivemq.cloud";
-    uint16_t port = 8883;
-}
